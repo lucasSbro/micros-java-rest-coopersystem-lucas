8cc5f71f9c66cb63959f3f03f8572ded45513880
lucas.sbroglia <lucas.sbroglia@navita.com.br>
2023-03-05 23:42:36 -0300

    refatorando.

--- code-test-quarkus-lucas/src/main/java/org/cooper/system/dto/Value.java
+++
@@ -1,9 +0,0 @@
-package org.cooper.system.dto;
-
-import java.math.BigDecimal;
-
-public class Value {
-    BigDecimal cotacaoCompra;
-    BigDecimal cotacaoVenda;
-    String dataHoraCotacao;
-}
--- code-test-quarkus-lucas/src/main/java/org/cooper/system/dto/ValueCotacao.java
+++
@@ -1,7 +0,0 @@
-package org.cooper.system.dto;
-
-public class ValueCotacao {
-    public String context;
-
-    public Value value;
-}
